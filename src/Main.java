4c30e93f0b301fbf8c45cdf2553f2d1dd7956170
annie8881 <880801blue@gmail.com>
2017-09-19 12:03:00 +0800

    0918上課作業

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("學號:106022062");
+        System.out.println("系級:行動商務與多媒體應用學系");
+        System.out.println("姓名:林姿如");
+        System.out.println("時間:2017/9/19");
+        System.out.print("地點:I319");
+    }
+}
